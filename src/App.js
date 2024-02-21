2ae6e1d595acc2297b33768fb880f6edf4c2907e
arafat-rifat <arafat15-12603@diu.edu.bd>
2024-02-22 03:55:42 +0600

    Working on From with onSubmit Handle From submit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -24,29 +25,49 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
 
-function handleSubmit(e){
-  e.preventDefault();
-  console.log(e)
-}
+  const [quantity, setQuantity] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+     
+    // create new Object
+      if (!description) return;
+    const newItem = { description, quantity , packed: false, id: Date.now() };
+
+    console.log(newItem);
+
+    // To set initial state 
+
+    setDescription('');
+    setQuantity(1);
+
+  }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-       
-       <select>
-        {Array.from({length: 20}, (_, i) => i + 1).map((num) => (
+
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
-            </option>
+          </option>
         ))}
-       </select>
+      </select>
 
-       <input type="text" placeholder="Item..."/>
-        
-        <button>Add</button>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
 
+      <button>Add</button>
     </form>
   );
 }
@@ -65,13 +86,13 @@ function PackingList() {
 
 function Item({ item }) {
   return (
-  <li>
-    <span style={item.packed ? {textDecoration : "line-through"} : {}}>
-      {item.quantity}
-      {item.description}
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
       </span>
       <button>❌</button>
-  </li>
+    </li>
   );
 }
 
